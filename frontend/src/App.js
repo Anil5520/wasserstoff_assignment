4f3adac8ccc63ffc3e348c1579419d268a162558
Anil5520 <anilmahto812@gmail.com>
2022-11-20 22:22:20 +0530

    basic working implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import Dashboard from './components/dashboard/dashboard';
-import Landing from './components/landing/landing';
 import Login from './components/login/login';
 import Register from './components/register/register';
 import AddTopic from './components/addTopic/addTopic';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      < Dashboard />
-      < Landing />
-      < Login />
-      < Register />
-      < AddTopic />
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/addtopic" element={<AddTopic />} />
+
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Navigate replace to="/login" />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
